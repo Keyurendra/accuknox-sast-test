c6ee69e95327cb3129aab855a3ff90154e0aadb5
Keyurendra <keyurendrareddy@gmail.com>
2026-08-25 18:11:43 +0530

    Add intentional security issues for SAST testing

--- container-app/app.js
+++ container-app/app.js
@@ -1,10 +1,17 @@
 const http = require('http');
 const PORT = 8080;
 
+// Intentional security issues for SonarQube testing
+const password = "SuperSecret123";
+
 const requestHandler = (req, res) => {
+  const userInput = req.url;
+
+  // Intentional code injection vulnerability
+  eval(userInput);
+
   res.end('Hello from AccuKnox demo container!');
 };
-
 
 const server = http.createServer(requestHandler);
 server.listen(PORT, () => {
