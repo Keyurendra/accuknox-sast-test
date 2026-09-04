3b3c50f9d1e78ae87a37f88dd7597a9afcc0c775
Keyurendra <keyurendrareddy@gmail.com>
2026-09-04 15:21:50 +0000

    Add temporary secret for Jenkins scan test

--- secrets_test.py
+++ secrets_test.py
@@ -19,3 +19,6 @@ API_KEY = "f8b3c7a9d2e1f4b6c8a0e3d5f7b9c2a4e6d8f0b2c4a6e8d0f2b4c6a8e0d2f4b6"
 print("This file contains fake hardcoded secrets for testing only.")
 
 # Temporary secret for Jenkins/TruffleHog testing
+
+# Temporary TruffleHog test secret
+GITHUB_TOKEN = "ghp_ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
